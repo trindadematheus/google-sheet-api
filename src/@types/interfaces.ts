974029cede55c68c7efed464b9c6b769e1a811aa
Matheus Trindade <trindadematheus27@gmail.com>
2021-04-30 11:38:31 -0300

    cadastrando dados na planilha

---
+++ src/@types/interfaces.ts
@@ -0,0 +1,6 @@
+export interface SpreadSheetProps {
+  spreadsheet_id: string;
+  client_email: string;
+  private_key: string;
+  data: object;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #666;
+  font-size: 46px;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export default function Home() {
+  return <Title>Google API</Title>
+}
